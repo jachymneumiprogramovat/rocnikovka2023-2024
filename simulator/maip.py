fdd897b905ee7d9db3fe9f3432cb465013bd3de9
Jáchym L <jachym.lowenhoffer@gmail.com>
2023-10-10 15:53:11 +0200

    zatím nefungující základy simulace

---
+++ simulator/maip.py
@@ -0,0 +1,6 @@
+from simulator import Simulator
+import math
+simulator = Simulator(
+    velocity=2,angle=math.radians(80),spin=-120,CoSF=0.2,VRC=0.9,Radius=0.025,D=0,AMC=0.25
+    )
+simulator.Simulate()
---
+++ simulator/simulator.py
@@ -0,0 +1,32 @@
+import math as math
+
+class Simulator:
+    def __init__(
+            self,velocity=0, angle=0, spin=0, CoSF=0, VRC=0, Radius=0, D=0, AMC=0
+            ) -> None:
+        self.angle = angle
+        self.velocity = velocity
+        self.Vx1 = math.cos(angle) * velocity
+        self.Vy1 = math.sin(angle) * velocity
+        print("angle:",math.degrees(self.angle))
+        print("Vx1:",self.Vx1,"Vy1:",self.Vy1)
+        self.spin = spin
+        #koefitients
+        self.CoSF = CoSF
+        self.VRC = VRC
+        self.Radius = Radius
+        self.D = D
+        self.AMC = AMC
+    
+    def Simulate(self):
+        print(self.velocity,self.spin)
+        print("spin:",self.get_SlidingSpin(),"velocity:",self.get_SlidingVx2())
+
+
+    def get_SlidingVx2(self):
+        rebound_speed= self.Vx1 - self.CoSF * (1 + self.VRC) * self.Vy1
+        return rebound_speed
+    
+    def get_SlidingSpin(self):
+        rebound_spin = self.spin + (1 + self.VRC) * (self.CoSF -(self.D/self.Radius)) * self.Vy1/(self.AMC*self.Radius)
+        return rebound_spin
